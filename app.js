3add51968b8148118f464debdbc26cd85257447c
Vladhex12 <cloutsfor@gmail.com>
2022-09-15 14:58:58 +0100

    Stage Uodates

--- app.js
+++ app.js
@@ -11,10 +11,42 @@ function UI() {}
 //add event listeners to get information from html.
 
 
-document.getElementById('book-form').addEventListener('submit', function(e) {
-    const title = document.getElementById('title').value
-    const author = document.getElementById('author').value
-    const isbn = document.getElementById('isbn').value
+UI.prototype.addBook - function(book){
+    const listItems = document.getElementById('book-list');
+    const row = document.createElement('tr')
+    row.innerHTML - `<td>${book.title}</td>`
+                    `<td>${book.title}</td>`
+                    `<td>${book.author}</td>`
+                    `<td>${book.isbn}</td>`
+                    `<td><a href"#">X<a/></td>`
+        listItems.appendChild(row)
+}
+
+
+UI.prototype.clearField = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+UI.prototype.alertMessage = function(message, className) {
+    const container = document.querySelector('.container');
+    const form = document.querySelector('form');
+    const div = document.createElement('div');
+    div.appendChild(document.createTextNode(message));
+    div.className = `alert${className}`;
+    container.insertBefore(div, form)
+
+    setTimeout(() => {
+        document.querySelector('.alert').remove()
+    }, 2000);
+}
+
+
+document.getElementById('book-form').addEventListener('submit', (e) => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
     console.log(title, author, isbn)
 
     const book = new Book(title, author , isbn)
@@ -22,23 +54,14 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     const ui = new UI();
 
     if(title === '' || author === ''|| isbn == ''){
-        ui.alertMessage('Pleasse fill all fields')
+        ui.alertMessage('Please fill all fields', 'Error')
+    }else{
+        ui.addBook(book)
+        ui.clearField()
+        ui.alertMessage('Your Book has been successfully added', 'success')
     }
 
     e.preventDefault();
-
-    UI.prototype.alertMessage = function(message, className) {
-        const container = document.querySelector('.container');
-        const form = document.querySelector('form');
-        const div = document.createElement('div');
-        div.appendChild(document.createTextNode(message));
-        div.className = `alert${className}`;
-        container.insertBefore(div, form)
-
-        setTimeout(() => {
-            document.querySelector('.alert').remove()
-        }, 2000);
-    }
 });
 
 
